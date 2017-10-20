4524f970c2f2d61a839b2ff99d4faa425daf8a88
avimehenwal <avi.mehanwal@gmail.com>
2017-10-20 18:53:37 +0530

    trim comments

--- ultrasonicSensor/ultrasonicSensor.ino
+++ ultrasonicSensor/ultrasonicSensor.ino
@@ -22,41 +22,27 @@ const int trigPin = 2;
 const int echoPin = 4;
 
 void setup() {
-  // initialize serial communication:
   Serial.begin(9600);
 }
 
-void loop()
-{
-  // establish variables for duration of the ping, 
-  // and the distance result in inches and centimeters:
+void loop() {
   long duration, inches, cm;
 
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
+  pinMode(trigPin, OUTPUT);digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
-
-  // convert the time into a distance
   inches = microsecondsToInches(duration);
   cm = microsecondsToCentimeters(duration);
   
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
+  Serial.print(duration);Serial.print("  ");
+  Serial.print(inches);Serial.print("in, ");
+  Serial.print(cm);Serial.print("cm");
   Serial.println();
-  
   delay(100);
 }
 
