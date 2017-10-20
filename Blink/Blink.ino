673028a24dea38b49cc491278f60858474b2c97c
avimehenwal <avi.mehanwal@gmail.com>
2017-10-20 16:10:59 +0530

    blink with serial console output

---
+++ Blink/Blink.ino
@@ -0,0 +1,36 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  Pin 13 has an LED connected on most Arduino boards.
+
+  Arduino programs are called SKETCHE.
+  PINs could be configured as I/P or O/P.
+  Digiatal and Analog PINs
+  
+ the setup routine runs once when you press reset: Runs ONLY once on powerup/restart
+ initialises and setup the initial values.
+*/
+
+int led = 13;
+void setup() {                
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);      // open the serial port at 9600 bps:
+  Serial.print(digitalRead(led));  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  Serial.print("------- NEW CYCLE -------");
+  Serial.print(digitalRead(led));
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.print(digitalRead(led));
+  
+  delay(500);  // wait for a second
+  Serial.print(digitalRead(led));
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  Serial.print(digitalRead(led));
+  delay(1000);               // wait for a second
+  
+  Serial.println("");      // prints another carriage return
+}
