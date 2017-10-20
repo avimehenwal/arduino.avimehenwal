1f074ad8f19cfd40e01f54a3b8d3a9732183f0a0
avimehenwal <avi.mehanwal@gmail.com>
2017-10-20 18:43:05 +0530

    ultrasonic sensor

--- external_led/external_led.ino
+++ external_led/external_led.ino
@@ -1,26 +1,29 @@
 /*
-  Control LED connected to PIN 9(+) and 8(-)
-  9 - Anode
-  8 - Cathode
+  Control LED connected to PIN anode(+) and cathode(-)
+  13 - Anode
+  12 - Cathode
 */
+
+int anode = 13;
+int cathode = 12;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("--Restart--");
-  Serial.print(digitalRead(9));Serial.print(digitalRead(8));Serial.print("\t");
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
-  Serial.print(digitalRead(9));Serial.print(digitalRead(8));Serial.print("\t");
-  digitalWrite(9, HIGH);
-  digitalWrite(8, LOW);
-  Serial.print(digitalRead(9));Serial.print(digitalRead(8));Serial.print("\t");
+  Serial.print(digitalRead(anode));Serial.print(digitalRead(cathode));Serial.print("\t");
+  pinMode(anode, OUTPUT);
+  pinMode(cathode, OUTPUT);
+  Serial.print(digitalRead(anode));Serial.print(digitalRead(cathode));Serial.print("\t");
+  digitalWrite(anode, HIGH);
+  digitalWrite(cathode, LOW);
+  Serial.print(digitalRead(anode));Serial.print(digitalRead(cathode));Serial.print("\t");
   Serial.println("");
 }
 
 void loop(void) {
   delay(1000);
-  digitalWrite(9, LOW);
+  digitalWrite(anode, LOW);
   delay(1000);
-  digitalWrite(9, HIGH);
+  digitalWrite(anode, HIGH);
 }
 
