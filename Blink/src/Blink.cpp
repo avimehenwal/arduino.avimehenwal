91cc9c6fed8b620fbb520fed31779b4af7996724
avimehenwal <avi.mehanwal@gmail.com>
2019-12-13 15:22:02 +0100

    Blink LED_BUILTIN with tests

--- Blink/src/Blink.cpp
+++ Blink/src/Blink.cpp
@@ -1,25 +1,22 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-
 #include <Arduino.h>
 
 void setup()
 {
-  // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("LED Number : ");
+  Serial.println(LED_BUILTIN); 
+  Serial.print(digitalRead(LED_BUILTIN)); 
+  
+  pinMode(12, OUTPUT);
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
+  Serial.print(digitalRead(LED_BUILTIN));
   delay(1000);
-  // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
+  Serial.print(digitalRead(LED_BUILTIN));
   delay(1000);
 }
