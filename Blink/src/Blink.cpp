fe95939990598c2de0e2c48538c48f560da5babf
avimehenwal <avi.mehanwal@gmail.com>
2019-12-13 22:42:40 +0100

    Refactor relay signal GPIO and wait value

--- Blink/src/Blink.cpp
+++ Blink/src/Blink.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 
+#define RELAY 7
+#define WAIT 9000
+// const int RELAY = 7;
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -8,15 +12,18 @@ void setup()
   Serial.println(LED_BUILTIN); 
   Serial.print(digitalRead(LED_BUILTIN)); 
   
-  pinMode(12, OUTPUT);
+  pinMode(RELAY, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.print(digitalRead(LED_BUILTIN));
-  delay(1000);
+  digitalWrite(RELAY, HIGH);
+  delay(WAIT);
+
   digitalWrite(LED_BUILTIN, LOW);
   Serial.print(digitalRead(LED_BUILTIN));
-  delay(1000);
+  digitalWrite(RELAY, LOW);
+  delay(WAIT);
 }
