1231c9475e69c80c2b7cb618244ce3e74ba9e8c5
avimehenwal <avi.mehanwal@gmail.com>
2020-01-04 22:03:31 +0100

    laser KY-008 module

---
+++ KY-008 LASER TRANSMITTER MODULE/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+int laserPin = 13;
+
+void setup() {                
+	pinMode(laserPin, OUTPUT);  // Define the digital output interface pin 13 
+}
+
+void loop() {
+	digitalWrite(laserPin, HIGH); // Open the laser head
+	delay(1000); // Delay one second
+	digitalWrite(laserPin, LOW); // Close the laser head
+	delay(1000); 
+}
--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Arduino Programming Language
 2. [DTH11 - temperature and humidity sensor](http://www.circuitbasics.com/wp-content/uploads/2015/11/DHT11-Datasheet.pdf)
 3. [KY-037 sound sensor](https://datasheet4u.com/datasheet-parts/KY-037-datasheet.php?id=1402047)
    1. Decible meter
-4. 
+4. Laser Diod
